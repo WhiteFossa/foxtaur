f0b5529deb695c40a3781304cbf03075b7408146
White Fossa <whitefossa@protonmail.com>
2022-12-27 19:56:21 +0100

    Indices for all selected buttons

--- Foxtaur/Foxtaur.Desktop/ViewModels/MoreSettingsViewModel.cs
+++ Foxtaur/Foxtaur.Desktop/ViewModels/MoreSettingsViewModel.cs
@@ -1,3 +1,5 @@
+using ReactiveUI;
+
 namespace Foxtaur.Desktop.ViewModels;
 
 /// <summary>
@@ -5,4 +7,49 @@ namespace Foxtaur.Desktop.ViewModels;
 /// </summary>
 public class MoreSettingsViewModel : ViewModelBase
 {
+    #region Bound properties
+    
+    private int _surfaceRunModeForwardButtonIndex;
+    private int _surfaceRunModeBackwardButtonIndex;
+    private int _surfaceRunModeTurnLeftButtonIndex;
+    private int _surfaceRunModeTurnRightButtonIndex;
+    
+    /// <summary>
+    /// Surface run mode forward button index
+    /// </summary>
+    public int SurfaceRunModeForwardButtonIndex
+    {
+        get => _surfaceRunModeForwardButtonIndex;
+        set => this.RaiseAndSetIfChanged(ref _surfaceRunModeForwardButtonIndex, value);
+    }
+    
+    /// <summary>
+    /// Surface run mode backward button index
+    /// </summary>
+    public int SurfaceRunModeBackwardButtonIndex
+    {
+        get => _surfaceRunModeBackwardButtonIndex;
+        set => this.RaiseAndSetIfChanged(ref _surfaceRunModeBackwardButtonIndex, value);
+    }
+    
+    /// <summary>
+    /// Surface run mode turn left button index
+    /// </summary>
+    public int SurfaceRunModeTurnLeftButtonIndex
+    {
+        get => _surfaceRunModeTurnLeftButtonIndex;
+        set => this.RaiseAndSetIfChanged(ref _surfaceRunModeTurnLeftButtonIndex, value);
+    }
+    
+    /// <summary>
+    /// Surface run mode turn right button index
+    /// </summary>
+    public int SurfaceRunModeTurnRightButtonIndex
+    {
+        get => _surfaceRunModeTurnRightButtonIndex;
+        set => this.RaiseAndSetIfChanged(ref _surfaceRunModeTurnRightButtonIndex, value);
+    }
+    
+    #endregion
+    
 }
--- Foxtaur/Foxtaur.Desktop/Views/MoreSettingsWindow.axaml.cs
+++ Foxtaur/Foxtaur.Desktop/Views/MoreSettingsWindow.axaml.cs
@@ -1,12 +1,46 @@
 using System.Collections.Generic;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 
 namespace Foxtaur.Desktop.Views;
 
 public partial class MoreSettingsWindow : Window
 {
+    /// <summary>
+    /// Keys, available to select as keyboard controls
+    /// </summary>
+    private readonly List<Key> AvailableKeys = new List<Key>()
+    {
+        Key.A,
+        Key.B,
+        Key.C,
+        Key.D,
+        Key.E,
+        Key.F,
+        Key.G,
+        Key.H,
+        Key.I,
+        Key.J,
+        Key.K,
+        Key.L,
+        Key.M,
+        Key.N,
+        Key.O,
+        Key.P,
+        Key.Q,
+        Key.R,
+        Key.S,
+        Key.T,
+        Key.U,
+        Key.V,
+        Key.W,
+        Key.X,
+        Key.Y,
+        Key.Z
+    };
+    
     public MoreSettingsWindow()
     {
         InitializeComponent();
@@ -23,18 +57,18 @@ public partial class MoreSettingsWindow : Window
         
         // Forward
         var surfaceRunModeForwardButtonCombobox = this.Find<ComboBox>("surfaceRunForwardButton");
-        surfaceRunModeForwardButtonCombobox.Items = new List<char>() { 'A', 'S', 'D', 'F' };
+        surfaceRunModeForwardButtonCombobox.Items = AvailableKeys;
         
         // Back
         var surfaceRunModeBackButtonCombobox = this.Find<ComboBox>("surfaceRunBackButton");
-        surfaceRunModeBackButtonCombobox.Items = new List<char>() { 'A', 'S', 'D', 'F' };
+        surfaceRunModeBackButtonCombobox.Items = AvailableKeys;
         
         // Left
-        var surfaceRunModeLeftButtonCombobox = this.Find<ComboBox>("surfaceRunLeftButton");
-        surfaceRunModeLeftButtonCombobox.Items = new List<char>() { 'A', 'S', 'D', 'F' };
+        var surfaceRunModeTurnLeftButtonCombobox = this.Find<ComboBox>("surfaceRunTurnLeftButton");
+        surfaceRunModeTurnLeftButtonCombobox.Items = AvailableKeys;
         
         // Right
-        var surfaceRunModeRightButtonCombobox = this.Find<ComboBox>("surfaceRunRightButton");
-        surfaceRunModeRightButtonCombobox.Items = new List<char>() { 'A', 'S', 'D', 'F' };
+        var surfaceRunModeTurnRightButtonCombobox = this.Find<ComboBox>("surfaceRunTurnRightButton");
+        surfaceRunModeTurnRightButtonCombobox.Items = AvailableKeys;
     }
 }
