250a88d1164a2ad99c960859632a0c419acade31
White Fossa <whitefossa@protonmail.com>
2022-12-30 09:49:38 +0100

    Gorica map

--- Foxtaur/Foxtaur.LibWebClient/Services/Implementations/WebClientRawStub.cs
+++ Foxtaur/Foxtaur.LibWebClient/Services/Implementations/WebClientRawStub.cs
@@ -45,27 +45,31 @@ public class WebClientRawStub : IWebClientRaw
 
     public async Task<LocationDto> GetLocationByIdAsync(Guid id)
     {
-        var startLocationId = new Guid("6550C9C5-6945-40F1-BDC6-17898C116A32");
-        var foxLocationId = new Guid("FEAA7806-7FFC-4CD8-A584-6B41B17A0E77");
-        var finishLocationId = new Guid("53ECF004-F388-4623-AABC-486BE60B6AC8");
-
-        
         switch (id.ToString().ToUpperInvariant())
         {
             case "6550C9C5-6945-40F1-BDC6-17898C116A32":
-                // Start
-                return new LocationDto(startLocationId, "Start", LocationType.Start, 54.7717312.ToRadians(), -39.8320896.ToRadians(), null);
-                break;
-            
+                // Davydovo - Start
+                return new LocationDto(new Guid("6550C9C5-6945-40F1-BDC6-17898C116A32"), "Start", LocationType.Start, 54.7717312.ToRadians(), -39.8320896.ToRadians(), null);
+
             case "FEAA7806-7FFC-4CD8-A584-6B41B17A0E77":
-                // Fox
-                return new LocationDto(foxLocationId, "Foxtaurs village", LocationType.Fox, 54.7684903.ToRadians(), -39.8525598.ToRadians(), new Guid("FC7BB34B-F9F0-4E7A-98D1-7699CC1B4423"));
-                break;
-            
+                // Davydovo - Fox
+                return new LocationDto(new Guid("FEAA7806-7FFC-4CD8-A584-6B41B17A0E77"), "Foxtaurs village", LocationType.Fox, 54.7684903.ToRadians(), -39.8525598.ToRadians(), new Guid("FC7BB34B-F9F0-4E7A-98D1-7699CC1B4423"));
+
             case "53ECF004-F388-4623-AABC-486BE60B6AC8":
-                // Finish
-                return new LocationDto(finishLocationId, "Finish", LocationType.Finish, 54.79184839.ToRadians(), -39.86736020.ToRadians(), null);
-                break;
+                // Davydovo - Finish
+                return new LocationDto(new Guid("53ECF004-F388-4623-AABC-486BE60B6AC8"), "Finish", LocationType.Finish, 54.79184839.ToRadians(), -39.86736020.ToRadians(), null);
+
+            case "D2ADFE4A-38D2-472F-A79C-6D3A6A257B6C":
+                // Gorica - Start
+                return new LocationDto(new Guid("D2ADFE4A-38D2-472F-A79C-6D3A6A257B6C"), "Start", LocationType.Start, 42.4499615.ToRadians(), -19.2651843.ToRadians(), null);
+            
+            case "9D448CD1-ADED-43C5-9513-53386548BFCB":
+                // Gorica - Fox
+                return new LocationDto(new Guid("9D448CD1-ADED-43C5-9513-53386548BFCB"), "Foxtaurs village", LocationType.Fox, 42.4484845.ToRadians(), -19.2744524.ToRadians(), new Guid("FC7BB34B-F9F0-4E7A-98D1-7699CC1B4423"));
+            
+            case "003062D4-1347-48DA-9193-F90652B09A7E":
+                // Gorica - Finish
+                return new LocationDto(new Guid("003062D4-1347-48DA-9193-F90652B09A7E"), "Finish", LocationType.Finish, 42.4496250.ToRadians(), -19.2662307.ToRadians(), null);
             
             default:
                 throw new ArgumentException("Location not found");
@@ -74,41 +78,63 @@ public class WebClientRawStub : IWebClientRaw
 
     public async Task<MapDto> GetMapByIdAsync(Guid id)
     {
-        var mapId = new Guid("2754AEB3-9E20-4017-8858-D4E5982D3802");
-        
-        if (id != mapId)
+        switch (id.ToString().ToUpperInvariant())
         {
-            throw new ArgumentException("Map not found!");
-        }
+            case "2754AEB3-9E20-4017-8858-D4E5982D3802":
+                return new MapDto(new Guid("2754AEB3-9E20-4017-8858-D4E5982D3802"),
+                    "Давыдово",
+                    54.807812457.ToRadians(),
+                    54.757759918.ToRadians(),
+                    -39.879142801.ToRadians(),
+                    -39.823302090.ToRadians(),
+                    @"Maps/Davydovo/Davydovo.tif.zst");
 
-        return new MapDto(mapId,
-            "Давыдово",
-            54.807812457.ToRadians(),
-            54.757759918.ToRadians(),
-            -39.879142801.ToRadians(),
-            -39.823302090.ToRadians(),
-            @"Maps/Davydovo/Davydovo.tif.zst");
+            case "2947B1E8-E54F-4C47-80E3-1A1E8AC045F7":
+                return new MapDto(new Guid("2947B1E8-E54F-4C47-80E3-1A1E8AC045F7"),
+                    "Gorica",
+                    42.454572697.ToRadians(),
+                    42.440712652.ToRadians(),
+                    -19.281242689.ToRadians(),
+                    -19.262488444.ToRadians(),
+                    @"Maps/Gorica/Gorica.tif.zst");
+
+            default:
+                throw new ArgumentException("Wrong ID");
+        }
     }
 
     public async Task<DistanceDto> GetDistanceByIdAsync(Guid id)
     {
-        var distanceId = new Guid("89E7EC2D-C7E3-42B6-BBB8-C340E681FCBE");
-
-        if (id != distanceId)
+        switch (id.ToString().ToUpperInvariant())
         {
-            throw new ArgumentException("Distance not found!");
-        }
+            case "89E7EC2D-C7E3-42B6-BBB8-C340E681FCBE":
+                return new DistanceDto(
+                    new Guid("89E7EC2D-C7E3-42B6-BBB8-C340E681FCBE"),
+                    "Давыдово",
+                    new Guid("2754AEB3-9E20-4017-8858-D4E5982D3802"),
+                    true,
+                    new Guid("6550C9C5-6945-40F1-BDC6-17898C116A32"),
+                    new Guid("53ECF004-F388-4623-AABC-486BE60B6AC8"),
+                    new List<Guid> { new Guid("FEAA7806-7FFC-4CD8-A584-6B41B17A0E77") },
+                    new List<Guid> { new Guid("E7B81F14-5B4E-446A-9892-36B60AF6511E") }
+                );
 
-        return new DistanceDto(
-            distanceId,
-            "Давыдово",
-            new Guid("2754AEB3-9E20-4017-8858-D4E5982D3802"),
-            true,
-            new Guid("6550C9C5-6945-40F1-BDC6-17898C116A32"),
-            new Guid("53ECF004-F388-4623-AABC-486BE60B6AC8"),
-            new List<Guid> { new Guid("FEAA7806-7FFC-4CD8-A584-6B41B17A0E77") },
-            new List<Guid> { new Guid("E7B81F14-5B4E-446A-9892-36B60AF6511E") }
-        );
+            case "A59E6C8F-4C5E-47B4-9EF2-8D1B25CD569C":
+                return new DistanceDto(
+                    new Guid("A59E6C8F-4C5E-47B4-9EF2-8D1B25CD569C"),
+                    "Gorica",
+                    new Guid("2947B1E8-E54F-4C47-80E3-1A1E8AC045F7"),
+                    true,
+                    new Guid("D2ADFE4A-38D2-472F-A79C-6D3A6A257B6C"),
+                    new Guid("003062D4-1347-48DA-9193-F90652B09A7E"),
+                    new List<Guid> { new Guid("9D448CD1-ADED-43C5-9513-53386548BFCB") },
+                    new List<Guid> { new Guid("E7B81F14-5B4E-446A-9892-36B60AF6511E") }
+                );
+                break;
+            
+            default:
+                throw new ArgumentException("Wrong ID");
+        }
     }
 
     public async Task<IReadOnlyCollection<DistanceDto>> ListDistancesAsync()
@@ -123,6 +149,17 @@ public class WebClientRawStub : IWebClientRaw
                 new Guid("6550C9C5-6945-40F1-BDC6-17898C116A32"),
                 new Guid("53ECF004-F388-4623-AABC-486BE60B6AC8"),
                 new List<Guid> { new Guid("FEAA7806-7FFC-4CD8-A584-6B41B17A0E77") },
+                new List<Guid> { new Guid("E7B81F14-5B4E-446A-9892-36B60AF6511E") }
+            ),
+            
+            new DistanceDto(
+                new Guid("A59E6C8F-4C5E-47B4-9EF2-8D1B25CD569C"),
+                "Gorica",
+                new Guid("2947B1E8-E54F-4C47-80E3-1A1E8AC045F7"),
+                true,
+                new Guid("D2ADFE4A-38D2-472F-A79C-6D3A6A257B6C"),
+                new Guid("003062D4-1347-48DA-9193-F90652B09A7E"),
+                new List<Guid> { new Guid("9D448CD1-ADED-43C5-9513-53386548BFCB") },
                 new List<Guid> { new Guid("E7B81F14-5B4E-446A-9892-36B60AF6511E") }
             )
         };
